513cb9a4f660dff2a4b3812a940982bf26e2f95f
garrettBourque <bourque.garrett@gmail.com>
2021-05-18 16:41:02 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName='bob';
+const leastFavoriteCustomer='knob';
+
+function upperCaseCustomerName(){
+    customerName=customerName.toUpperCase();
+}
+
+function setBestCustomer(name='not bob'){
+    bestCustomer=name;
+}
+
+function overwriteBestCustomer(name){
+    bestCustomer=name;
+}
+
+function changeLeastFavoriteCustomer(name){
+    leastFavoriteCustomer=name;
+}
